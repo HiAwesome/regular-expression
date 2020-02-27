6934b3eb1c0f42ad1fe38311fff03a574c401eee
moqi <moqimoqidea@gmail.com>
2020-02-27 10:13:39 +0800

    Test .a.\.xls

---
+++ src/main/java/book/chapter02/A07DotADotBackSlashDotXls.java
@@ -0,0 +1,25 @@
+package book.chapter02;
+
+import lombok.extern.slf4j.Slf4j;
+import tool.Tool;
+
+import java.util.List;
+
+import static book.chapter02.A00Data.FILE_NAME_LIST_1;
+
+/**
+ * @author moqi
+ * On 2/27/20 10:01
+ */
+@Slf4j
+public class A07DotADotBackSlashDotXls {
+
+    /**
+     * 2020-02-27 10:13:21 INFO  A07DotADotBackSlashDotXls:22 - list:[na1.xls, na2.xls, sa2.xls]
+     */
+    public static void main(String[] args) {
+        List<String> list = Tool.patternList(".a.\\.xls", FILE_NAME_LIST_1);
+        log.info("list:{}", list);
+    }
+
+}
