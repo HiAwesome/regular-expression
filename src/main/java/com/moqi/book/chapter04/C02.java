dccb3342566966a006381284a058855f1fd72ca3
moqi <moqimoqidea@gmail.com>
2020-02-27 10:40:22 +0800

    Test myArray\[0\]

---
+++ src/main/java/com/moqi/book/chapter04/C02.java
@@ -0,0 +1,25 @@
+package com.moqi.book.chapter04;
+
+import com.moqi.tool.Tool;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+import static com.moqi.data.Data.REGULAR_TEXT_3;
+
+/**
+ * @author moqi
+ * On 2/27/20 10:37
+ */
+@Slf4j
+public class C02 {
+
+    /**
+     * 2020-02-27 10:39:59 INFO  C02:22 - list:[myArray[0]]
+     */
+    public static void main(String[] args) {
+        List<String> list = Tool.patternString("myArray\\[0\\]", REGULAR_TEXT_3);
+        log.info("list:{}", list);
+    }
+
+}
