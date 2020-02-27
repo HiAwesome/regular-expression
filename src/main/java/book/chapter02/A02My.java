aa0e87960ef14a86793dcd13f74c711e1c0ff3d7
moqi <moqimoqidea@gmail.com>
2020-02-27 10:02:01 +0800

    Test my

---
+++ src/main/java/book/chapter02/A02My.java
@@ -0,0 +1,23 @@
+package book.chapter02;
+
+import lombok.extern.slf4j.Slf4j;
+import tool.Tool;
+
+import java.util.List;
+
+import static book.chapter02.A00Data.HELLO;
+
+/**
+ * @author moqi
+ * On 2/27/20 10:01
+ */
+@Slf4j
+public class A02My {
+
+    public static void main(String[] args) {
+        List<String> list = Tool.patternString("my", HELLO);
+        // 2020-02-27 10:01:27 INFO  A02My:20 - list:[my, my]
+        log.info("list:{}", list);
+    }
+
+}
