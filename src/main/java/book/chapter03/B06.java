31ec32f54fb5b1ed3b733d3cb3f308adc6c43ee6
moqi <moqimoqidea@gmail.com>
2020-02-27 10:31:43 +0800

    Test [ns]a[^0-9]\.xls

---
+++ src/main/java/book/chapter03/B06.java
@@ -0,0 +1,25 @@
+package book.chapter03;
+
+import lombok.extern.slf4j.Slf4j;
+import tool.Tool;
+
+import java.util.List;
+
+import static data.Data.FILE_NAME_LIST_4;
+
+/**
+ * @author moqi
+ * On 2/27/20 10:18
+ */
+@Slf4j
+public class B06 {
+
+    /**
+     * 2020-02-27 10:31:25 INFO  B06:22 - list:[sam.xls]
+     */
+    public static void main(String[] args) {
+        List<String> list = Tool.patternList("[ns]a[^0-9]\\.xls", FILE_NAME_LIST_4);
+        log.info("list:{}", list);
+    }
+
+}
