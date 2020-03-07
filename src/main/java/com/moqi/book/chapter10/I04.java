b2ad1a9a2e112e0aa1a342cb73bf9a2fdf88cb05
moqi <moqimoqidea@gmail.com>
2020-03-07 22:19:44 +0800

    Test \d{5}(-\d{4})?

---
+++ src/main/java/com/moqi/book/chapter10/I04.java
@@ -0,0 +1,29 @@
+package com.moqi.book.chapter10;
+
+import com.moqi.tool.Tool;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+import static com.moqi.data.Data.TEXT_38_TXT;
+
+/**
+ * Java 不支持正则表达式嵌入条件
+ *
+ * @author moqi
+ * On 2/27/20 10:18
+ */
+@Slf4j
+public class I04 {
+
+    private static final String REGEX = "\\d{5}(-\\d{4})?";
+
+    /**
+     * 2020-03-07 22:19:24 INFO  I04:38 - list:[11111, 22222, 33333, 44444-4444]
+     */
+    public static void main(String[] args) {
+        List<String> list = Tool.patternString(REGEX, TEXT_38_TXT);
+        log.info("list:{}", list);
+    }
+
+}
